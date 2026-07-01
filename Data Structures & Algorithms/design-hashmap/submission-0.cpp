b24be565d232f1e794ec9f4ea325bf8fceb463d2
kjhq <arvintro@gmail.com>
2026-07-01 14:03:02 +0530

    Add: design-hashmap - submission-0

---
+++ Data Structures & Algorithms/design-hashmap/submission-0.cpp
@@ -0,0 +1,28 @@
+class MyHashMap {
+private:
+    int bucket[1000001];
+public:
+    MyHashMap() {
+        std::fill(bucket, bucket + 1000001, -1);
+    }
+    
+    void put(int key, int value) {
+        bucket[key] = value;
+    }
+    
+    int get(int key) {
+        return bucket[key];
+    }
+    
+    void remove(int key) {
+        bucket[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
